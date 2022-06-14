6505d3a0e3cea4cf58c9f5dee4287dcf754d3ef9
MacielDouglas <douglasmaciel.dm@gmail.com>
2022-06-14 14:35:00 -0300

    Adicionado testes de erros e executado plano de teste

--- NutriVitta/CalculadoraIMC/CalculadorIMC/TestXUnit/UnitTest1.cs
+++ NutriVitta/CalculadoraIMC/CalculadorIMC/TestXUnit/UnitTest1.cs
@@ -34,10 +34,36 @@ namespace TestXUnit
         [Theory]
         [InlineData(1.68, 2.82, 68, 24.11)]
         [InlineData(1.55, 2.40, 43, 17.91)]
-        [InlineData(2.05, 4.20, 137, 32.62)] // este dara erro. O IMC correto é 32.61
+        [InlineData(2.05, 4.20, 137, 32.61)]
         [InlineData(1.77, 3.13, 92, 29.39)] 
 
         public void TestCalculadoraIMCLista(double primeiroNumero, double resultadoMult, double segundoNumero, double IMC)
+        {
+            //Act - Ação
+
+            var resultado = IMCalcular.Calcular.multiplicar(primeiroNumero);
+
+            // var resultDivisao = Calculadora.Divisao(resultado, segundoNumero);
+            var resultDivisao = IMCalcular.Calcular.dividir(segundoNumero, Math.Round(resultado, 2, MidpointRounding.ToZero));
+            var classeImc = IMCalcular.Calcular.classificacao(Math.Round(resultDivisao, 2, MidpointRounding.ToZero));
+
+            //Assert - Verificação
+            Assert.Equal(resultadoMult, Math.Round(resultado, 2, MidpointRounding.ToZero));
+            Assert.Equal(IMC, Math.Round(resultDivisao, 2, MidpointRounding.ToZero));
+            Assert.Equal(IMC, Math.Round(classeImc, 2, MidpointRounding.ToZero));
+        }
+
+        //TESTE DE ERROS
+        // O programa não roda com caracteres 
+
+        [Theory]
+        [InlineData(1.688, 2.849344, 68, 23.865142)] // erro foi adicionado muitos numeros decimais com a matemática correta.
+        [InlineData(1.51, 2.41, 41, 17.91)] // erro foi trocado alguns numeros de forma aleatoria
+        [InlineData(-2.05, 4.20, 137, 32.61)] // adicionado um valor negativo
+        [InlineData(1*77, 3.13, 93, 29.39)] // retirado caracteres, e adicionado sinal multiplicação '*'
+        [InlineData(-1.77, 3.1, 9*3, 29.39)] // retirado caracteres e numeros negativos 
+
+        public void TesteERROSCalculadora(double primeiroNumero, double resultadoMult, double segundoNumero, double IMC)
         {
             //Act - Ação
 
